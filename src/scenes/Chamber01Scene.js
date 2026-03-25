dc31edf7d9d15dc70dc9ce7c599a74fb5c7bb1bc
Herbachino1776 <jjojhjnjh@gmail.com>
2026-03-25 12:03:25 -0400

    fix: retarget dev button to Pressure Deacon chamber

--- src/scenes/Chamber01Scene.js
+++ src/scenes/Chamber01Scene.js
@@ -425,10 +425,10 @@ export class Chamber01Scene extends Phaser.Scene {
       return;
     }
 
-    // Temporary Sector 2 Chamber 3 shortcut so the Chamber 01 mobile DEV button can jump straight to the current Kiln of Judgement test room.
+    // Temporary Sector 2 Chamber 2 shortcut so the Chamber 01 mobile DEV button can jump straight to the current Pressure Deacon test room.
     this.cleanupSceneUi?.();
     this.audioDirector?.shutdown();
-    this.scene.start('Sector02Chamber03Scene', {
+    this.scene.start('Sector02Chamber02Scene', {
       devWarp: true,
       source: this.scene.key
     });
