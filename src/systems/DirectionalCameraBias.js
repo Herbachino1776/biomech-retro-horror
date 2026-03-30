bacc7360a5f02a138411cfc4348a61191c9ff6fd
Herbachino1776 <jjojhjnjh@gmail.com>
2026-03-30 11:55:34 -0400

    Strengthen movement-driven directional camera bias

--- src/systems/DirectionalCameraBias.js
+++ src/systems/DirectionalCameraBias.js
@@ -3,25 +3,54 @@ import Phaser from 'phaser';
 const DEFAULTS = {
   desktopLookAheadX: 56,
   portraitLookAheadX: 24,
+  lookAheadBoostMultiplier: 1.75,
   lerpFactor: 0.14,
-  movementThreshold: 16
+  directionLerpFactor: 0.16,
+  movementThresholdEnter: 26,
+  movementThresholdExit: 14,
+  idleSettleDelayMs: 260
 };
 
 export class DirectionalCameraBias {
-  constructor({ camera, player, desktopBaseOffsetX, portraitBaseOffsetX, desktopLookAheadX, portraitLookAheadX, lerpFactor, movementThreshold }) {
+  constructor({
+    camera,
+    player,
+    desktopBaseOffsetX,
+    portraitBaseOffsetX,
+    desktopLookAheadX,
+    portraitLookAheadX,
+    lookAheadBoostMultiplier,
+    lerpFactor,
+    directionLerpFactor,
+    movementThreshold,
+    movementThresholdEnter,
+    movementThresholdExit,
+    idleSettleDelayMs
+  }) {
     this.camera = camera;
     this.player = player;
     this.desktopBaseOffsetX = desktopBaseOffsetX;
     this.portraitBaseOffsetX = portraitBaseOffsetX;
-    this.desktopLookAheadX = desktopLookAheadX ?? DEFAULTS.desktopLookAheadX;
-    this.portraitLookAheadX = portraitLookAheadX ?? DEFAULTS.portraitLookAheadX;
+
+    const resolvedBoostMultiplier = lookAheadBoostMultiplier ?? DEFAULTS.lookAheadBoostMultiplier;
+    this.desktopLookAheadX = (desktopLookAheadX ?? DEFAULTS.desktopLookAheadX) * resolvedBoostMultiplier;
+    this.portraitLookAheadX = (portraitLookAheadX ?? DEFAULTS.portraitLookAheadX) * resolvedBoostMultiplier;
+
     this.lerpFactor = lerpFactor ?? DEFAULTS.lerpFactor;
-    this.movementThreshold = movementThreshold ?? DEFAULTS.movementThreshold;
+    this.directionLerpFactor = directionLerpFactor ?? DEFAULTS.directionLerpFactor;
+
+    const legacyMovementThreshold = movementThreshold ?? null;
+    this.movementThresholdEnter = movementThresholdEnter ?? legacyMovementThreshold ?? DEFAULTS.movementThresholdEnter;
+    this.movementThresholdExit = movementThresholdExit ?? Math.min(this.movementThresholdEnter, DEFAULTS.movementThresholdExit);
+    this.idleSettleDelayMs = idleSettleDelayMs ?? DEFAULTS.idleSettleDelayMs;
 
     this.isPortrait = false;
     this.followOffsetY = 0;
-    this.lastDirection = player?.facing === -1 ? -1 : 1;
+    this.lastMovementDirection = player?.facing === -1 ? -1 : 1;
+    this.smoothedDirection = this.lastMovementDirection;
     this.currentOffsetX = desktopBaseOffsetX;
+    this.isTravelingHorizontally = false;
+    this.stationaryTimeMs = 0;
   }
 
   setLayout({ isPortrait, followOffsetY }) {
@@ -41,11 +70,32 @@ export class DirectionalCameraBias {
     }
 
     const velocityX = this.player.body?.velocity?.x ?? 0;
-    if (Math.abs(velocityX) >= this.movementThreshold) {
-      this.lastDirection = Math.sign(velocityX) || this.lastDirection;
-    } else if (this.player.facing === -1 || this.player.facing === 1) {
-      this.lastDirection = this.player.facing;
+    const speedX = Math.abs(velocityX);
+
+    if (this.isTravelingHorizontally) {
+      if (speedX <= this.movementThresholdExit) {
+        this.isTravelingHorizontally = false;
+      }
+    } else if (speedX >= this.movementThresholdEnter) {
+      this.isTravelingHorizontally = true;
     }
+
+    if (this.isTravelingHorizontally) {
+      const resolvedDirection = Math.sign(velocityX);
+      if (resolvedDirection !== 0) {
+        this.lastMovementDirection = resolvedDirection;
+      }
+      this.stationaryTimeMs = 0;
+    } else {
+      const deltaMs = this.player?.scene?.game?.loop?.delta ?? 16.67;
+      this.stationaryTimeMs += deltaMs;
+    }
+
+    const desiredDirection = this.stationaryTimeMs > this.idleSettleDelayMs
+      ? 0
+      : this.lastMovementDirection;
+
+    this.smoothedDirection = Phaser.Math.Linear(this.smoothedDirection, desiredDirection, this.directionLerpFactor);
 
     const targetOffsetX = this.getTargetOffsetX();
     this.currentOffsetX = Phaser.Math.Linear(this.currentOffsetX, targetOffsetX, this.lerpFactor);
@@ -55,7 +105,7 @@ export class DirectionalCameraBias {
   getTargetOffsetX() {
     const baseOffset = this.isPortrait ? this.portraitBaseOffsetX : this.desktopBaseOffsetX;
     const lookAhead = this.isPortrait ? this.portraitLookAheadX : this.desktopLookAheadX;
-    return baseOffset - this.lastDirection * lookAhead;
+    return baseOffset - this.smoothedDirection * lookAhead;
   }
 
   getFollowOffsetX() {
